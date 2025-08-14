ea9e309387e9c28daf7db4d06c9ecb64bc7af124
sawaniThiwandika <thiwandika.whs02@gmail.com>
2025-08-14 09:58:08 +0530

    create new interface for library items

---
+++ src/interfaces/LibraryItem.ts
@@ -0,0 +1,10 @@
+export interface LibraryItem {
+    id: string;
+    title: string;
+    feedRate: number;
+    time: number;
+    size: string;
+    description: string;
+    gcode: string;
+    image: string;
+}
